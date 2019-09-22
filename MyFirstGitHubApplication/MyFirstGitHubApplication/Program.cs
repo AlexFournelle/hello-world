618b28bcab568000ac1e81ccfd76dc9adcb1cc35
AlexFournelle <afournelle@gmail.com>
2019-09-22 17:02:54 -0400

    Keep the console open (console.read)

--- MyFirstGitHubApplication/MyFirstGitHubApplication/Program.cs
+++ MyFirstGitHubApplication/MyFirstGitHubApplication/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace MyFirstGitHubApplication
 {
@@ -10,7 +7,8 @@ namespace MyFirstGitHubApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World")
+            Console.WriteLine("Hello World");
+            Console.Read(); // So as to keep the console open
         }
     }
 }
