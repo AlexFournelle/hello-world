a4b2cf8f5f2dcf4021046005623a989e18f00ece
AlexFournelle <afournelle@gmail.com>
2019-09-22 18:31:03 -0400

    Ajout premier projet (Projet1_chaines) + ch. README

---
+++ Proj1_chaines/Proj1_chaines/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+
+namespace Proj1_chaines
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Declaration de l'unique parametre
+            int repetition;
+
+            // Saisie de la valeur de repetition
+            Console.WriteLine("Entrez un nombre de répétitions, svp: ");
+            repetition = Convert.ToInt32(Console.ReadLine());
+
+            // Initialisation d'un stringbuilder
+            StringBuilder sb = new StringBuilder();
+            
+            // Les boucles imbriquees qui font tout le travail
+            for (int i=1; i<=repetition; i++) {
+                for (int i2=0; i2<i; i2++) { 
+                    sb.Append('*');
+                }
+                sb.Append('#');
+            }
+
+            // Output du resultat
+            Console.WriteLine(sb.ToString());
+            Console.Read(); // So as to keep the console open
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 The obligatory noob repository
 
 Looking around to get a sense for how it all goes.  Only ever just messed with svn (not git) way too many years ago.
+
+MyFirstGitHubApplication
+	Ce que ça dit sur la boîte: ma première appli sur GitHub. Un Hello World des plus drabes. Hey, c'est aussi ma
+	première appli C#
+
+Proj1_chaines
+	Un premier exercice de java que je refais en C# question d'explorer ce langage. Il s'agit de générer une chaine 
+	répétitive de caractères # et * selon un entier reçu en paramètre (l'énoncé est dans le répertoire concerné)
